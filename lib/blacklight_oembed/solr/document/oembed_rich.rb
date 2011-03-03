1fc4dd8dd5583429606c30e93534fcb77d6b8eec
Chris Beer <chris_beer@wgbh.org>
2011-03-03 09:30:16 -0500

    render oembed rich html (using an ugly hack\!)

--- lib/blacklight_oembed/solr/document/oembed_rich.rb
+++ lib/blacklight_oembed/solr/document/oembed_rich.rb
@@ -6,19 +6,28 @@ module BlacklightOembed::Solr::Document::OembedRich
   def self.register_export_formats(document)
   end
 
-  def to_oembed
-    {
+  def to_oembed(oembed)
+    semantic_values = self.to_semantic_values
+    ({
       :version => '1.0',
       :type => oembed_type,
       :provider_name => '',
       :provider_url => '',
       :title => semantic_values[:title],
       :author_name => semantic_values[:author],
-      :html => ''
-    }
+      :html => to_oembed_html
+    }).merge(oembed)
   end
 
   def oembed_type
     'rich'
+  end
+
+  def to_oembed_html
+    foo = ActionView::Base.new
+    foo.view_paths = ApplicationController.view_paths
+    foo.extend ApplicationHelper
+    # foo.send :format, 'html'
+    foo.render_document_partial(self, 'oembed')
   end
 end
